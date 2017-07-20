d92b47f5e29fed752345610bc7be2ee6dc16629c
antweer <trajwani@gmail.com>
2017-07-20 03:23:56 -0500

    Tested NoteList component

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+  return (
+    <div>
+      <h5>You have no notes</h5>
+      <p>You should think about creating a note</p>
+    </div>
+  );
+}
+
+export default NoteListEmptyItem;
