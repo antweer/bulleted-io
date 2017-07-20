4556bf8c7a63f80e65a7849de0ac4f20abc7cd70
antweer <trajwani@gmail.com>
2017-07-19 21:34:42 -0500

    Created and tested note removal and note updating method

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import moment from 'moment';
+import SimpleSchema from 'simpl-schema';
 
 export const Notes = new Mongo.Collection('notes');
 
@@ -15,6 +16,50 @@ Meteor.methods({
       body: '',
       userId: this.userId,
       updatedAt: moment().valueOf()
+    });
+  },
+  'notes.remove'(_id){
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({ _id });
+    
+    Notes.remove({ _id, userId: this.userId });
+  },
+  'notes.update'(_id, updates) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      },
+      title: {
+        type: String,
+        optional: true
+      },
+      body: {
+        type: String,
+        optional: true
+      }
+    }).validate({
+      _id,
+      ...updates
+    });
+    
+    Notes.update(_id, {
+      $set: {
+        updatedAt: moment().valueOf(),
+        ...updates
+      }
     });
   }
 });
